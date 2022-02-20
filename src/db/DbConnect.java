a61ce4f503c17c30f638cfc10310a3d125846176
vishal <vishal@hp14-bf120tu>
2022-02-20 10:18:03 +0530

    added remaining files

---
+++ src/db/DbConnect.java
@@ -0,0 +1,27 @@
+package db;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class DbConnect {
+    public static Connection c;
+    public static Statement st;
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/eatery", "vishal", "password");
+            st = c.createStatement();
+        }
+        catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+    
+    public static void closeConnection() {
+        try {
+            c.close();
+        }
+        catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+}
